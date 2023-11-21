fdabb4a4784443f421eb3a49d9eea20ae1925bbf
GuillaumeLeGoff <legoff.guill@gmail.com>
2023-11-21 17:30:15 +0100

    gui boucle

--- Display/src/App.js
+++ Display/src/App.js
@@ -1,100 +1,74 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import TruckList from "./Components/TruckList";
 import MediaDisplay from "./Components/MediaDisplay";
-import {useEffect, useState} from "react";
+import dataService from "./services/dataService";
+
 function App() {
-    const [showTrucks, setShowTrucks] = useState(true);
-    const [mediaIndex, setMediaIndex] = useState(0);
-    const [medias, setMedias] = useState([]);
-    const [totalTruckPages, setTotalTruckPages] = useState(1);
-    const [currentTruckPage, setCurrentTruckPage] = useState(0);
-    const [fetchedTrucks, setFetchedTrucks] = useState([]);
-    const [settings, setSettings] = useState([]);
-    const [debutVeille, setDebutVeille] = useState(0);
-    const [finVeille, setFinVeille] = useState(0);
-    const [dureeDefilement, setDureeDefilement] = useState(30);
+  const [trucks, setTrucks] = useState([]);
+  const [medias, setMedias] = useState([]);
+  const [mediaIndex, setMediaIndex] = useState(-1);
+  const [truckIndex, setTruckIndex] = useState(0);
+  const [intervalDuration, setIntervalDuration] = useState(2000); // Default to 10 seconds
 
-    const fetchSettings = async () => {
-        try {
-            const response = await fetch('http://localhost:4000/settings');
-            const data = await response.json();
-            setSettings(data);
-            setDebutVeille(settings.debutVeille);
-            setFinVeille(settings.finVeille);
-            setDureeDefilement(settings.dureeDefilement);
-            console.log("App.fetchSettings: data:", data);
-        } catch (error) {
-            console.error('Error fetching settings:', error);
-        }
-
+  // Fetch data from server
+  const fetchData = async () => {
+    try {
+      const [trucksData, mediasData] = await Promise.all([
+        dataService.getTrucks(),
+        dataService.getMedias(),
+      ]);
+      setTrucks(trucksData);
+      setMedias(mediasData);
+    } catch (error) {
+      console.error("An error occurred while fetching data:", error);
     }
+  };
 
-    const fetchTrucks = async () => {
-        try {
-            const response = await fetch('http://localhost:4000/camions');
-            const data = await response.json();
-            setFetchedTrucks(data);
-            console.log("App.fetchTrucks: data:", data);
-            const totalPages = Math.ceil(data.length / 10);
-        } catch (error) {
-            console.error('Error fetching trucks:', error);
-        }
-    };
+  // Fetch data on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const handleMediaEnd = () => {
-        setMediaIndex((prevIndex) => {
-            const nextIndex = (prevIndex + 1) % medias.length;
-            if (nextIndex === 0) {
-                setShowTrucks(true); // Retourner à l'affichage des camions
-                setCurrentTruckPage(0);
-                fetchTrucks(); // Refetch les camions
-            }
-            return nextIndex;
-        });
-    };
-
-    const fetchMedias = async () => {
-        try {
-            const response = await fetch('http://localhost:4000/media-management/');
-            const data = await response.json();
-            setMedias(data);
-            console.log("App.fetchMedias: data:", data);
-        } catch (error) {
-            console.error('Error fetching medias:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchTrucks(); // Fetch initial des camions
-        fetchMedias(); // Fetch initial des médias
-    }, []);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            if (showTrucks) {
-                setCurrentTruckPage((prevPage) => {
-                    const nextPage = (prevPage + 1) % totalTruckPages;
-                    if (nextPage === 0 && medias.length > 0) {
-                        setShowTrucks(false);
-                        setMediaIndex(0);
-                        fetchMedias();
-                    }
-                    return nextPage;
-                });
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setMediaIndex((prevIndex) => {
+ 
+        if (prevIndex >= medias.length - 1 ) {
+          setIntervalDuration(2000); // Set duration to 10 seconds for trucks
+          setTruckIndex((prevTruckIndex) => {
+            console.log("Previous truck index:", prevTruckIndex);
+            if (prevTruckIndex >= trucks.length - 10) {
+                console.log("Looping back to start"+ prevIndex);
+              return 0; // Loop back to start
             } else {
-                handleMediaEnd(); // Utilisez votre fonction handleMediaEnd ici
+                console.log("Moving to next group of trucks"+ prevIndex);
+              return prevTruckIndex + 10; // Move to next group of trucks
             }
-        }, 10000);
+          });
+          console.log("Moving to trucks"+ prevIndex);
+          return -1;  // Loop back to show trucks
+        } else {
+            console.log("Moving to next media"+ prevIndex);
+          setIntervalDuration(medias[prevIndex + 1].duration * 1000); // Set duration to media duration
+          return prevIndex + 1;
+        }
+      });
+    }, intervalDuration);
 
-        return () => clearInterval(intervalId);
-    }, [showTrucks, mediaIndex, medias.length, totalTruckPages]);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [medias, intervalDuration, trucks]);
 
-    return (
-        <div className="App">
-            {showTrucks ? <TruckList setCurrentPage={setCurrentTruckPage} setTotalPages={setTotalTruckPages} currentPage={currentTruckPage} fetchedTrucks={fetchedTrucks}/> :
-                <MediaDisplay media={medias[mediaIndex] || null} onMediaEnd={handleMediaEnd}/>}
-        </div>
-    );
+  return (
+    <div className="App">
+      {mediaIndex === -1 ? (
+        <TruckList trucks={trucks.slice(truckIndex, truckIndex + 10)} />
+      ) : (
+        <MediaDisplay media={medias[mediaIndex]} />
+      )}
+    </div>
+  );
 }
 
 export default App;

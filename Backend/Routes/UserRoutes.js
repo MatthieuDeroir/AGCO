ab7be6f3b30a0b311aca05e3dfa492c45eb1aeba
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-12 11:33:54 +0100

    .env config

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Models/UserModel');
 const jwt = require('jsonwebtoken');
-const { key } = require ('../config');
 
 // Inscription
 router.post('/signup', async (req, res) => {
@@ -11,8 +10,8 @@ router.post('/signup', async (req, res) => {
         await user.save();
 
         // Créer un token JWT
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY); // Remplacez 'SECRET_KEY' par une clé secrète robuste
-        res.status(201).send({ token });
+        const token = jwt.sign({_id: user._id}, process.env.SECRET_KEY); // Remplacez 'SECRET_KEY' par une clé secrète robuste
+        res.status(201).send({token});
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -21,14 +20,14 @@ router.post('/signup', async (req, res) => {
 // Connexion
 router.post('/signin', async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
+        const user = await User.findOne({username: req.body.username});
         if (!user || !bcrypt.compareSync(req.body.password, user.password)) {
             return res.status(400).send('Invalid username or password');
         }
 
         // Créer un token JWT
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY); // Remplacez 'SECRET_KEY' par une clé secrète robuste
-        res.status(200).send({ token });
+        const token = jwt.sign({_id: user._id}, process.env.SECRET_KEY); // Remplacez 'SECRET_KEY' par une clé secrète robuste
+        res.status(200).send({token});
     } catch (error) {
         res.status(500).send(error.message);
     }

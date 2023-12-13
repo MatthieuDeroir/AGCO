a16550a23e280870e99f909380e22a9a70532e83
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-13 10:20:10 +0100

    tools electron

--- Display/src/App.js
+++ Display/src/App.js
@@ -30,7 +30,8 @@ function App() {
     const [trucks, setTrucks] = useState([]);
     const [medias, setMedias] = useState([]);
     const [settings, setSettings] = useState([]);
-    const [mediaIndex, setMediaIndex] = useState(-1);
+    const [mediaIndex, setMediaIndex] = useState(0);
+    const [isMediaDisplay, setIsMediaDisplay] = useState(false);
     const [truckIndex, setTruckIndex] = useState(0);
     const [resetInterval, setResetInterval] = useState(false);
     const [intervalDuration, setIntervalDuration] = useState(30000); // Default to 30 seconds
@@ -83,6 +84,77 @@ function App() {
         return trucks.slice(startIndex, startIndex + 10);
     };
 
+    const updateDisplay = () => {
+        const currentTime = new Date().toLocaleTimeString();
+        const isVeille = isWithinVeilleTime(currentTime, settings.debutVeille, settings.finVeille) || (!hasTrucks && !hasMedias);
+
+        if (isVeille) {
+            setIsMediaDisplay(false);
+            setMediaIndex(0);
+            setTruckIndex(0);
+            // Ici, vous pouvez définir un état pour afficher l'écran de veille
+        } else if (hasMedias && !hasTrucks) {
+            setIsMediaDisplay(true);
+            cycleMedias();
+        } else if (hasTrucks && !hasMedias) {
+            setIsMediaDisplay(false);
+            cycleTrucks();
+        } else if (hasTrucks && hasMedias) {
+            // Alterner entre les camions et les médias
+            if (isMediaDisplay) {
+                cycleMedias();
+            } else {
+                cycleTrucks();
+            }
+        }
+    };
+
+    const cycleTrucks = () => {
+        if ((truckIndex + 1) * 10 < trucks.length) {
+            setTruckIndex(truckIndex + 1);
+        } else {
+            setTruckIndex(0);
+            setIsMediaDisplay(true); // Passer aux médias après les camions
+        }
+        setIntervalDuration(settings.dureeDefilement * 1000);
+    };
+
+    const cycleMedias = () => {
+        if (mediaIndex < medias.length - 1) {
+            setMediaIndex(mediaIndex + 1);
+        } else {
+            setMediaIndex(0);
+            setIsMediaDisplay(false); // Revenir aux camions après les médias
+        }
+        setIntervalDuration(medias[mediaIndex]?.duration * 1000 || 2000);
+    };
+
+
+    const updateDisplay = () => {
+        const currentTime = new Date().toLocaleTimeString();
+        const isVeille = isWithinVeilleTime(currentTime, settings.debutVeille, settings.finVeille) || (!hasTrucks && !hasMedias);
+
+        if (isVeille) {
+            setIsMediaDisplay(false);
+            setMediaIndex(0);
+            setTruckIndex(0);
+            // Ici, vous pouvez définir un état pour afficher l'écran de veille
+        } else if (hasMedias && !hasTrucks) {
+            setIsMediaDisplay(true);
+            cycleMedias();
+        } else if (hasTrucks && !hasMedias) {
+            setIsMediaDisplay(false);
+            cycleTrucks();
+        } else if (hasTrucks && hasMedias) {
+            // Alterner entre les camions et les médias
+            if (isMediaDisplay) {
+                cycleMedias();
+            } else {
+                cycleTrucks();
+            }
+        }
+    };
+
 
     // useEffect(() => {
     //     const timer = setInterval(() => {
@@ -129,29 +201,40 @@ function App() {
     //     };
     // }, [resetInterval, medias, settings, truckIndex, trucks ? trucks.length : 0]);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         fetchData().then(() => console.log("Data refreshed"));
+    //
+    //         // Check if more truck pages are available
+    //         if ((truckIndex + 1) * 10 < trucks.length) {
+    //             // Move to the next truck page
+    //             setTruckIndex(truckIndex + 1);
+    //         } else {
+    //             // Reset to the first truck page or switch to media
+    //             setTruckIndex(0);
+    //
+    //             if (medias && medias.length > 0) {
+    //                 // If media available, switch to media
+    //                 setMediaIndex(0);
+    //                 setIntervalDuration(medias[mediaIndex]?.duration * 1000 || 2000); // Duration for first media
+    //             }
+    //             else {
+    //                 // If no media, continue displaying trucks
+    //                 setIntervalDuration(settings.dureeDefilement * 1000); // Duration for trucks
+    //                 setMediaIndex(-1);
+    //             }
+    //         }
+    //     }, intervalDuration);
+
+        useEffect(() => {
             fetchData().then(() => console.log("Data refreshed"));
+            const timer = setInterval(() => {
+                updateDisplay();
+            }, intervalDuration);
 
-            // Check if more truck pages are available
-            if ((truckIndex + 1) * 10 < trucks.length) {
-                // Move to the next truck page
-                setTruckIndex(truckIndex + 1);
-            } else {
-                // Reset to the first truck page or switch to media
-                setTruckIndex(0);
+            return () => clearInterval(timer);
+        }, [intervalDuration, trucks, medias, settings, truckIndex, mediaIndex, isMediaDisplay]);
 
-                if (medias && medias.length > 0) {
-                    // If media available, switch to media
-                    setIntervalDuration(medias[0]?.duration * 1000 || 2000); // Duration for first media
-                    setMediaIndex(0);
-                } else {
-                    // If no media, continue displaying trucks
-                    setIntervalDuration(settings.dureeDefilement * 1000); // Duration for trucks
-                    setMediaIndex(-1);
-                }
-            }
-        }, intervalDuration);
 
         return () => clearInterval(timer);
     }, [resetInterval, medias, settings, truckIndex, trucks.length]);

efa713e8ab23ce4250f0d6247c06fb8aa3ea0dbe
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-13 11:01:58 +0100

    tools electron

--- Display/src/App.js
+++ Display/src/App.js
@@ -6,16 +6,39 @@ import './App.css';
 import Veille from "./Components/Veille";
 import moment from "moment";
 
+
+const DebugInfo = ({mediaIndex, intervalDuration, trucks, medias, settings}) => {
+    // Ensure trucks and medias are arrays before accessing their length
+    const trucksLength = Array.isArray(trucks) ? trucks.length : 0;
+    const mediasLength = Array.isArray(medias) ? medias.length : 0;
+
+    return (
+        <div style={{textAlign: 'left', margin: '10px', padding: '10px', border: '1px solid #ccc'}}>
+            <h4>Debug Information:</h4>
+            <p>Media Index: {mediaIndex}</p>
+            <p>Interval Duration: {intervalDuration}</p>
+            <p>Number of Trucks: {trucksLength}</p>
+            <p>Number of Medias: {mediasLength}</p>
+            <p>Settings: {JSON.stringify(settings)}</p>
+        </div>
+    );
+};
+
+
 function App() {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [time, setTime] = useState(0);
     const [trucks, setTrucks] = useState([]);
     const [medias, setMedias] = useState([]);
     const [settings, setSettings] = useState([]);
-    const [mediaIndex, setMediaIndex] = useState(0);
+    const [mediaIndex, setMediaIndex] = useState(-1);
     const [truckIndex, setTruckIndex] = useState(0);
-    const [isMediaDisplay, setIsMediaDisplay] = useState(false);
+    const [resetInterval, setResetInterval] = useState(false);
+    const [intervalDuration, setIntervalDuration] = useState(30000); // Default to 30 seconds
 
+    // Fetch data from server
     const fetchData = async () => {
+        //set time with current time
+        setTime(new Date().toLocaleTimeString());
         try {
             const [trucksData, mediasData, settingsData] = await Promise.all([
                 dataService.getTrucks(),
@@ -25,77 +48,100 @@ function App() {
             setTrucks(trucksData);
             setMedias(mediasData);
             setSettings(settingsData[0]);
-            setTime(new Date().toLocaleTimeString());
         } catch (error) {
             console.error("An error occurred while fetching data:", error);
         }
     };
 
+    // Fetch data on component mount
     useEffect(() => {
-        fetchData();
-
+        fetchData()
     }, []);
 
     const isWithinVeilleTime = (currentTime, startTime, endTime) => {
         const current = moment(currentTime, 'HH:mm');
         const start = moment(startTime, 'HH:mm');
         const end = moment(endTime, 'HH:mm');
+        console.log("Veille time ? " + startTime + endTime + current.isBetween(start, end));
         return current.isBetween(start, end);
     };
 
-    const updateDisplay = () => {
-        const currentTime = new Date().toLocaleTimeString();
-        const isVeille = isWithinVeilleTime(currentTime, settings.debutVeille, settings.finVeille) || (!trucks.length && !medias.length);
+    const hasTrucks = Array.isArray(trucks) && trucks.length > 0;
+    const hasMedias = Array.isArray(medias) && medias.length > 0;
 
-        if (isVeille) {
-            setIsMediaDisplay(false);
-            setMediaIndex(0);
-            setTruckIndex(0);
-        } else if (medias.length && !trucks.length) {
-            setIsMediaDisplay(true);
-            cycleMedias();
-        } else if (trucks.length && !medias.length) {
-            setIsMediaDisplay(false);
-            cycleTrucks();
-        } else if (trucks.length && medias.length) {
-            isMediaDisplay ? cycleMedias() : cycleTrucks();
+    const shouldDisplayVeille = !hasTrucks && !hasMedias;
+    const shouldDisplayMedias = !hasTrucks && hasMedias;
+    const isVeilleTime = isWithinVeilleTime(time, settings.debutVeille, settings.finVeille);
+
+    const getCurrentTruckPage = () => {
+        if (!Array.isArray(trucks) || trucks.length === 0) {
+            // Return an empty array if trucks is not an array or is empty
+            return [];
         }
+
+        const startIndex = truckIndex * 10;
+        return trucks.slice(startIndex, startIndex + 10);
     };
 
-    const cycleTrucks = () => {
-        setTruckIndex((truckIndex + 1) * 10 < trucks.length ? truckIndex + 1 : 0);
-        setIsMediaDisplay(truckIndex === 0);
-    };
-
-    const cycleMedias = () => {
-        setMediaIndex(mediaIndex < medias.length - 1 ? mediaIndex + 1 : 0);
-        setIsMediaDisplay(!(mediaIndex === medias.length - 1));
-    };
-
-    const isVeilleTime = (currentTime, startTime, endTime) => {
-        const current = moment(currentTime, 'HH:mm');
-        const start = moment(startTime, 'HH:mm');
-        const end = moment(endTime, 'HH:mm');
-        return current.isBetween(start, end);
-    }
 
     useEffect(() => {
         const timer = setInterval(() => {
-            fetchData();
-            updateDisplay();
-        }, settings.dureeDefilement); // Durée par défaut pour changer l'affichage
+            setMediaIndex((prevIndex) => {
+                fetchData().then(() => {
+                    console.log("Data refreshed");
+                });
+                if (prevIndex === -1) {
+                    // Check if more truck pages are available
+                    if ((truckIndex + 1) * 10 < trucks ? trucks.length : 0) {
+                        // Move to the next truck page
+                        setTruckIndex(truckIndex + 1);
+                        setIntervalDuration(settings.dureeDefilement * 1000); // Duration for trucks
+                        return -1;
+                    } else if (medias && medias.length > 0) {
+                        // Switch to media if available
+                        setTruckIndex(0); // Reset truck index
+                        setIntervalDuration(medias[0]?.duration * 1000 || 2000); // Duration for first media
+                        return 0;
+                    } else {
+                        // If no media, keep displaying trucks
+                        setTruckIndex(0); // Reset truck index
+                        return -1;
+                    }
+                } else if (prevIndex >= medias.length - 1) {
+                    // After the last media, show first truck page
 
-        return () => clearInterval(timer);
-    }, [truckIndex, mediaIndex, isMediaDisplay]);
+                    setIntervalDuration(settings.dureeDefilement * 1000); // Duration for trucks
+                    setTruckIndex(0); // Start from the first truck page
+                    return -1;
+                } else {
+                    // Move to the next media and set its duration
+                    const nextMediaDuration = medias[prevIndex + 1]?.duration * 1000 || 2000;
+                    setIntervalDuration(nextMediaDuration);
+                    return prevIndex + 1;
+                }
+            });
+        }, intervalDuration);
+
+        // Clean up the interval when the component unmounts
+        return () => {
+            clearInterval(timer);
+        };
+    }, [resetInterval, medias, settings, truckIndex, trucks ? trucks.length : 0]);
+
+
+    useEffect(() => {
+        setResetInterval(prev => !prev);
+    }, [mediaIndex, intervalDuration]);
+
 
     return (
         <div className="App">
-            {isVeilleTime(time, settings.debutVeille, settings.finVeille) || (!trucks.length && !medias.length) ? (
+            {isVeilleTime || shouldDisplayVeille ? (
                 <Veille/>
-            ) : isMediaDisplay ? (
+            ) : shouldDisplayMedias || mediaIndex !== -1 ? (
                 <MediaDisplay media={medias[mediaIndex]}/>
             ) : (
-                <TruckList trucks={trucks.slice(truckIndex * 10, (truckIndex + 1) * 10)} duration={settings.dureeDefilement}/>
+                <TruckList trucks={getCurrentTruckPage()} duration={settings.dureeDefilement}/>
             )}
         </div>
     );

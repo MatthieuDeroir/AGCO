463093a4333ebb29bbe5666bbfe923fb433b3ac6
GuillaumeLeGoff <legoff.guill@gmail.com>
2023-12-13 16:32:43 +0100

    640 => 600 and 320 => 300

--- Display/electron.js
+++ Display/electron.js
@@ -4,8 +4,8 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 320,
-        height: 640,
+        width: 300,
+        height: 600,
         webPreferences: {
             nodeIntegration: true,
             webSecurity: false,
